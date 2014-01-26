6ae9726bcc060b5280faf09f5c274736ff1f70bb
JoseOneTime <claveren@gmail.com>
2014-01-26 16:26:22 -0500

    Revise '/rango' page using a jade template

--- rango/views.py
+++ rango/views.py
@@ -1,8 +1,11 @@
 from django.http import HttpResponse
+from django.template import RequestContext
+from django.shortcuts import render_to_response
 
 def index(request):
-	return HttpResponse('Rango says Hello World! ' +
-		'<a href="/rango/about">About</a>')
+	context = RequestContext(request)
+	context_dict = {'boldmessage' : 'I am the bold message from the context!'}
+	return render_to_response('rango/index.jade', context_dict, context)
 
 def about(request):
 	return HttpResponse('Rango says "Here is the About page". ' +
