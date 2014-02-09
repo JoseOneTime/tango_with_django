d78cad0a68d9ee2d089c2d33812ccdb5c50092e8
JoseOneTime <claveren@gmail.com>
2014-02-08 23:35:38 -0500

    List up to 5 Categories on index page

--- rango/views.py
+++ rango/views.py
@@ -2,13 +2,14 @@ from django.http import HttpResponse
 from django.template import RequestContext
 from django.shortcuts import render_to_response
 
+from rango.models import Category
+
 def index(request):
 	context = RequestContext(request)
-	context_dict = {'boldmessage' : 'I am the bold message from the context!'}
+	category_list = Category.objects.order_by('-name')[:5]
+	context_dict = {'categories': category_list}
 	return render_to_response('rango/index.jade', context_dict, context)
 
 def about(request):
 	return HttpResponse('Rango says "Here is the About page". ' +
 		'<a href="/rango">Index</a>')
-
-
