16d4a5127eac29166e02d6ba4a7fb9436984c273
JoseOneTime <claveren@gmail.com>
2014-02-08 22:58:28 -0500

    Create models Category and Page

--- tango_with_django/settings.py
+++ tango_with_django/settings.py
@@ -69,7 +69,7 @@ TEMPLATE_LOADERS = (
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'rango.db'),
     }
 }
 
